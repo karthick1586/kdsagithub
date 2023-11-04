6097e44ce54b9ba787a73912de764223fb65c636
kdsa1586@gmail.com <kdsa1586@gmail.com>
2023-11-04 07:07:43 -0400

    Changes in Startup.cs file

--- src/SimpleAPI/Startup.cs
+++ src/SimpleAPI/Startup.cs
@@ -28,7 +28,7 @@ namespace SimpleAPI
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime and Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
